20b65ce749ecb1730cd15fa7fb22e12ba3568ab4
Dimarrh <chidimma3187@gmail.com>
2023-02-23 18:43:36 -0800

    fixed_problems

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - A function to check if is upper
+ * @c: a char
+ *
+ * Return: Always(0)
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - Check if the input is a number
+ * @c: a char
+ * Return: Always(0);
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
