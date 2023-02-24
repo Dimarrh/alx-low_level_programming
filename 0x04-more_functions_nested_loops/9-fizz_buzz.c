6b637d845ea72d8bd4acd318ebf1797fe2ef3418
Dimarrh <chidimma3187@gmail.com>
2023-02-23 20:28:08 -0800

    print_number.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,17 @@ int main(void)
 
 	while (i < 100)
 	{
-		if (i % 3 == 0)
+		if ((i % 3) == 0 && (i % 5) == 0)
 		{
-			printf("Fizz");
+			printf("Fizz Buzz");
 		}
 		else if ((i % 5) == 0)
 		{
 			printf("Buzz");
 		}
-		else if ((i % 3) == 0 && (i % 5) == 0)
+		else if ((i % 3) == 0)
 		{
-			printf("Fizz Buzz");
+			printf("Fizz");
 		}
 		else
 		{
