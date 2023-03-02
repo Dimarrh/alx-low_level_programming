9df4e2abdb6cd9d41dd77a9e9777e4a027933a84
Dimarrh <chidimma3187@gmail.com>
2023-03-02 15:36:55 -0800

    more_pointers_and_arrays

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
 /**
- *_strncpy - updates the value of the variable
- * the pointer points to 98
- * @dest: pointer to the variable to update
- * @src: pointer to the variable
- * @n: bytes to copy
- * Return: pointer to the updated variable
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
-char *_strncpy(char *dest, char *src, int n)
+int main(void)
 {
-int lengthOfDest = 0;
+    char s1[98];
+    char *ptr;
+    int i;
 
-while (lengthOfDest < n)
-{
-dest[lengthOfDest] = src[lengthOfDest];
-if (src[lengthOfDest] == '\0')
-{
-break;
-}
-lengthOfDest++;
-}
-
-return (dest);
+    for (i = 0; i < 98 - 1; i++)
+    {
+        s1[i] = '*';
+    }
+    s1[i] = '\0';
+    printf("%s\n", s1);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+    printf("%s\n", s1);
+    printf("%s\n", ptr);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+    printf("%s", s1);
+    printf("%s", ptr);
+    for (i = 0; i < 98; i++)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", s1[i]);
+    }
+    printf("\n");
+    return (0);
 }
