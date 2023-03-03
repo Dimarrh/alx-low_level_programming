e9605137fba0c36cec4244588d8d270b0cc04e55
Dimarrh <chidimma3187@gmail.com>
2023-03-02 16:20:38 -0800

    pointers_and_challenges

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,26 @@
 #include "main.h"
+
 /**
- *print_number - updates the value of the variable
- * the pointer points to 98
- * @n: pointer to the variable to update
- *
- * Return: pointer to the updated variable
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
  */
 
 void print_number(int n)
 {
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
-if (n >= 0 && n <= 9)
-{
-_putchar((n % 10) + '0');
-}
-else
-{
-print_number(n / 10);
-_putchar((n % 10) + '0');
-}
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
