39862b6e3309e903343c810b0da8b4b2103e7b40
Dimarrh <chidimma3187@gmail.com>
2023-03-10 20:32:50 -0800

    argc_tasks_is_fun

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of file
+ * @argc: number of arguments
+ * @argv: arguments entered
+ * Description: print name of file
+ * Return: zero
+ **/
+
+int main(int argc, char **argv)
+{
+argc = argc;
+printf("%s\n", argv[0]);
+
+return (0);
+}
