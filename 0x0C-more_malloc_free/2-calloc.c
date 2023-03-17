38deba55d76df01644798a466e168fe393744a79
Dimarrh <chidimma3187@gmail.com>
2023-03-16 19:37:45 -0700

    fixed_malloc_tasks

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+
+char *new_mem(char *, char, unsigned int);
+
+/**
+*** _calloc - allocates memory for an array **
+*** @nmemb: number of elements of the array **
+*** @size: bytes of elements of the array ****
+*** Description: the function allocates ******
+*** memory of size bytes for an array ********
+*** Return: pointer to created memory ********
+**/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *mem;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	mem = malloc(size * nmemb);
+
+	if (mem == NULL)
+	{
+		return (NULL);
+	}
+
+	new_mem(mem, 0, nmemb * size);
+
+	return (mem);
+}
+
+/**
+*** new_mem - copy a character into an array **
+*** @mem: array to be filled with character ***
+*** @c: character to be copied ****************
+*** @a: number of times to copy character *****
+*** Return: pointer to array created **********
+**/
+
+char *new_mem(char *mem, char c, unsigned int a)
+{
+	unsigned int i;
+
+	for (i = 0; i < a; i++)
+	{
+		mem[i] = c;
+	}
+
+	return (mem);
+}
